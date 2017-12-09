4df89e84a0b61e5a6c200aaa567e84fee8f3432b
Will <bottrellw@gmail.com>
2017-12-08 21:42:49 -0800

    Try using server.js

--- server.js
+++ server.js
@@ -3,17 +3,11 @@
 
  /* serves main page */
  app.get("/", function(req, res) {
-    console.log(req, res);
+    response.sendfile('client/index.html');
  });
 
-  app.post("/test", function(req, res) { 
-	/* some server side logic */
-	console.log('in test');
-	res.send("OK");
-  });
-
  var port = process.env.PORT || 5000;
- 
+
  app.listen(port, function() {
    console.log("Listening on " + port);
  });
