4b4e2a6b9081457a4753c24f71c7bb6785d84c7d
Will Bottrell <bottrellw@gmail.com>
2018-01-13 13:25:17 -0800

    Fixed node/express file serving

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
  var express = require("express");
  var app = express();
 
+ app.use(express.static(__dirname));
+ app.use(express.static(__dirname + "/bower_components"));
+ app.use(express.static(__dirname + "/client"));
+
  /* serves main page */
- app.get("*", function(req, res) {
-    res.sendFile("client/index.html", {root: '.'});
+ app.get('*', function(req, res) {
+     res.sendFile('./client/index.html', {root: '.'});
  });
 
  var port = process.env.PORT || 5000;
